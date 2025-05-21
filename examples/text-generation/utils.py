0849ae2f7b208b01236fb503c07c2e8558b878d8
Zhou, Huijuan <huijuan.zhou@intel.com>
2025-05-21 13:18:00 +0800

    add qwen3 family
    Co-authored-by: Wei Lin <wei2.lin@intel.com>

--- examples/text-generation/utils.py
+++ examples/text-generation/utils.py
@@ -532,7 +532,7 @@ def setup_distributed_model(args, model_dtype, model_kwargs, logger):
 
     model = deepspeed.init_inference(model, **ds_inference_kwargs)
     model = model.module
-    if model.config.model_type in ["llama", "falcon", "qwen2", "starcoder2", "gemma"]:
+    if model.config.model_type in ["llama", "falcon", "qwen2", "starcoder2", "gemma", "qwen3"]:
         patch_scoped_linear_all_reduce(model)
 
     if args.quant_config:
