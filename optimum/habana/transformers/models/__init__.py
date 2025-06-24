b6f4c97903b7976acf3576df68613ec095c3d683
tianyuan0211 <tian.yuan@intel.com>
2025-06-24 11:41:18 +0800

    update qwen3moe classification related files

--- optimum/habana/transformers/models/__init__.py
+++ optimum/habana/transformers/models/__init__.py
@@ -285,6 +285,8 @@ from .qwen3_moe import (
     GaudiQwen3MoeAttention,
     GaudiQwen3MoeDecoderLayer,
     GaudiQwen3MoeForCausalLM,
+    GaudiQwen3MoeForSequenceClassification,
+    GaudiQwen3MoeForTokenClassification,
     GaudiQwen3MoeMLP,
     GaudiQwen3MoeModel,
     GaudiQwen3MoeSparseMoeBlock,
